ca3fc294c6518ec0253ed710960dd1d9e61605de
Brian Spinos <brianspinos777@hotmail.com>
2022-09-17 19:02:19 -0700

    Create dom.js

---
+++ javascript/dom/dom.js
@@ -0,0 +1,11 @@
+const foo = document.getElementById('foo')
+const foo = document.getElementsByClassName('foo')
+const foo = document.getElementsByTagName('form')
+const foo = document.querySelector('foo')
+const foo = document.querySelectorAll('foo')
+
+
+// click, mouseover, mouseout, ...
+foo.addEventListener("click", (e) => {
+    // console.log(e);
+})
